d835d9f9e5dbfbfde8f7d74357412b90ec583433
zhhe11 <1125447881@qq.com>
2025-08-18 13:50:00 +0800

    save: 4090 train fail because of the limitation of the cuda memory

--- train_pi0.sh
+++ train_pi0.sh
@@ -1,11 +1,10 @@
 ## 训练指令
 python -m lerobot.scripts.train \
     --dataset.repo_id=metaworld \
-    --dataset.root= r"/media/xlab/My Book/dataset/metaworld_mt50" \
-    --policy.path= r"/media/xlab/My Book/dataset/pi0fast_ckpt" \
-    --policy.push_to_hub=False \
-    --policy.repo_id= r"/media/xlab/My Book/dataset/metaworld_mt50"
-
-
+    --dataset.root="/home/xlab/Dataset/metaworld_mt50" \
+    --policy.type=pi0fast \
+    --policy.checkpoint_path="/home/xlab/Checkpoint/pi0fast_ckpt" \
+    --policy.push_to_hub=false \
+    --policy.repo_id="/home/xlab/Dataset/metaworld_mt50" \
 
 
